02eb31ecef239dc0c6d822654fa3c650fb25e4e9
homlox1 <162463326+homlox1@users.noreply.github.com>
2024-03-06 20:10:17 +0100

    Update dehaze-recolor-hdr.js

--- plugins/dehaze-recolor-hdr.js
+++ plugins/dehaze-recolor-hdr.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				let error;
 				try {
 					const This = await processing(img, "dehaze");
-					conn.sendFile(m.chat, This, "", " instagram.com/noureddine_ouafy ...", m);
+					conn.sendFile(m.chat, This, "", " instagram.com/_othmaane ...", m);
 				} catch (er) {
 					error = true;
 				} finally {
